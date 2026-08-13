62026d3b3171f060de1e8838550cc73538980ce8
Alex Hall <earthbound19@users.noreply.github.com>
2026-08-13 14:54:44 -0600

    udp. to check for admin privilege and error out if no

--- scripts/bleachbit_clean_preset.bat
+++ scripts/bleachbit_clean_preset.bat
@@ -1,1 +1,14 @@
-"C:\Users\ahall\AppData\Local\BleachBit\bleachbit_console.exe" --clean --preset
+:: Check for administrator privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+	echo.
+	echo ! ==
+    echo ERROR: This script must be run as Administrator.
+    echo Please right-click on the script and select "Run as administrator",
+	echo or run it from a CMD prompt started with Administrator privileges.
+    echo.
+    pause
+    exit /b 1
+)
+
+"%userprofile%\AppData\Local\BleachBit\bleachbit_console.exe" --clean --preset
